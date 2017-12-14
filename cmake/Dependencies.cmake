9c7d192383fec28b78c81ef793098c2e6aea08b0
Yurii Batrak <yuriib@met.no>
2017-12-14 21:36:02 +0100

    be more agressive in disabling unused options

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -5,21 +5,34 @@ if(${USE_LOCAL_NETCDF})
     ExternalProject_add(HDF5
         URL ${CMAKE_CURRENT_SOURCE_DIR}/libs/hdf5-1.8.19.tar.bz2
         INSTALL_DIR ${CMAKE_BINARY_DIR}/libs
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DHDF5_BUILD_FORTRAN=ON
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+            -DHDF5_BUILD_FORTRAN=ON
+            -DBUILD_TESTING=OFF
+            -DHDF5_BUILD_TOOLS=OFF
+            -DHDF5_BUILD_EXAMPLES=OFF
+            -DHDF5_BUILD_CPP_LIB=OFF
         )
 
     ExternalProject_add(NetCDF_C
         DEPENDS HDF5
         URL ${CMAKE_CURRENT_SOURCE_DIR}/libs/netcdf-4.4.1.1.tar.gz
         INSTALL_DIR ${CMAKE_BINARY_DIR}/libs
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DENABLE_DAP=OFF
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+            -DENABLE_DAP=OFF
+            -DBUILD_UTILITIES=OFF
+            -DENABLE_EXAMPLES=OFF
+            -DENABLE_TESTS=OFF
         )
 
     ExternalProject_add(NetCDF_Fortran
         DEPENDS NetCDF_C
         URL ${CMAKE_CURRENT_SOURCE_DIR}/libs/netcdf-fortran-4.4.4.tar.gz
         INSTALL_DIR ${CMAKE_BINARY_DIR}/libs
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+            -DENABLE_TESTS=OFF
         )
 
     ExternalProject_get_property(NetCDF_Fortran install_dir)
@@ -38,7 +51,13 @@ if(${USE_LOCAL_GRIB_API})
     ExternalProject_add(grib_api
         URL ${CMAKE_CURRENT_SOURCE_DIR}/libs/grib_api-1.23.0-Source.tar.gz
         INSTALL_DIR ${CMAKE_BINARY_DIR}/libs
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DENABLE_NETCDF=OFF -DENABLE_FORTRAN=ON  -DENABLE_PYTHON=OFF
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+            -DENABLE_FORTRAN=ON
+            -DENABLE_NETCDF=OFF
+            -DENABLE_PYTHON=OFF
+            -DENABLE_EXAMPLES=OFF
+            -DENABLE_TESTS=OFF
         )
 
     ExternalProject_get_property(grib_api install_dir)
