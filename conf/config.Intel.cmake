805be0d825dd73e254e2be590e11784405eaf325
Yurii Batrak <yuriib@met.no>
2017-12-21 21:47:19 +0100

    provide compiler flags for DEBUG and RELEASE configurations

--- conf/config.Intel.cmake
+++ conf/config.Intel.cmake
@@ -1,2 +1,4 @@
-set (fort_flags "-convert big_endian -r8 -g -assume nosource_include -assume byterecl -fpic -traceback -fp-model source")
-set (CMAKE_Fortran_FLAGS "-O2 -fpe0 -ftz ${fort_flags}")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -convert big_endian -r8 -g -assume nosource_include -assume byterecl -fpic -traceback -fp-model source")
+
+set(CMAKE_Fortran_FLAGS_RELEASE "-O2 -fpe0 -ftz")
+set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -fp-stack-check -ftrapuv -fpe3 -fp-speculation=strict -check all")
